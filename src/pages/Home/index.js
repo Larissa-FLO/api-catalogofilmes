feb3cf53be9a3b0d2a808f2ed1723a5c766200f4
Larissa Lopes de Oliveira <larissa.arqueo@outlook.com>
2024-01-24 00:49:08 -0300

    correção

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,7 +14,7 @@ function Home() {
         }
       }; 
 
-    useEffect(() => {
+    useEffect((buscaFilme) => {
 
       fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', buscaFilme)
         .then(response => response.json())
